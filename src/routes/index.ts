2b7b078a5afe19d0d44474ab5b967687b3ca1e23
Maiga Mahamane Ahmad <mahamaneahmad1@gmail.com>
2026-05-26 07:06:19 +0200

    succesfully added the session feature

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,18 @@ import authRoutes from "./authRoutes";
 import healthRoutes from "./healthRoutes";
 import matchRoutes from "./matchRoutes";
 import userRoutes from "./userRoutes";
+import conversationRoutes from "./conversationRoutes";
+import sessionRoutes from "./sessionRoutes";
+import creditRoutes from "./creditRoutes";
 
 const router = Router();
 
-// Mount feature route files here. Example: router.use("/users", userRoutes);
 router.use("/", healthRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/matches", matchRoutes);
+router.use("/conversations", conversationRoutes);
+router.use("/sessions", sessionRoutes);
+router.use("/credits", creditRoutes);
 
 export default router;
